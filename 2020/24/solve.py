3a2fb0bb4a548fb4be05e130c278acaeda9b19ef
Anthony Martinet <anthony@alkivi.fr>
2020-12-26 13:52:52 +0100

    2020/24 part2

--- 2020/24/solve.py
+++ 2020/24/solve.py
@@ -33,87 +33,12 @@ def load_data():
     return data
 
 
-class Tile:
-    def __init__(self, x, y, floor):
-        self.x = x
-        self.y = y
-        self.floor = floor
-        self.color = True
-        self._neighboors = None
-        self.to_flip = False
-
-    @property
-    def index(self):
-        return f'{self.x}_{self.y}'
-
-    def flip(self):
-        self.color = not self.color
-
-    @property
-    def black(self):
-        return not self.color
-
-    @property
-    def white(self):
-        return self.color
-
-    @property
-    def white_neighboors(self):
-        total = 0
-        for neighboor in self.neighboors():
-            if neighboor.white:
-                total += 1
-        return total
-
-    def black_neighboors(self):
-        total = 0
-        for neighboor in self.neighboors():
-            if neighboor.black:
-                total += 1
-        return total
-
-    def will_flip(self):
-        self.to_flip = True
-
-    def update(self):
-        if self.to_flip:
-            self.color = not self.color
-            self.to_flip = False
-    
-    def neighboors(self, add_missing=True):
-        if self._neighboors is not None:
-            return self._neighboors
-
-        neighboors = []
-        for index in [
-                f'{self.x-1}_{self.y}',
-                f'{self.x}_{self.y-1}',
-                f'{self.x+1}_{self.y-1}',
-                f'{self.x+1}_{self.y}',
-                f'{self.x}_{self.y+1}',
-                f'{self.x-1}_{self.y+1}']:
-            if index in self.floor.tiles:
-                neighboors.append(self.floor.tiles[index])
-            else:
-                if add_missing:
-                    x = int(index.split('_')[0])
-                    y = int(index.split('_')[1])
-                    new_tile = Tile(x, y, self.floor)
-                    self.floor.to_add.append(new_tile)
-                    neighboors.append(new_tile)
-        self._neighboors = neighboors
-        return neighboors
-
-
 class Floor:
     def __init__(self, data):
         self.data = data
         self.tiles = collections.defaultdict(int)
 
     def setup(self):
-        x = 0
-        y = 0
-
         vectors = {
             'w': (-1, 0),
             'sw': (0, -1),
@@ -137,19 +62,9 @@ class Floor:
                 line = line[len(direction):]
 
             self.tiles[vector] = (self.tiles[vector] + 1) % 2
-        print(f'We have {len(self.tiles)} tiles and {len(self.data)} lines')
 
-
-    @property
-    def black_tiles(self):
-        return sum(filter(lambda x: x == 1, self.tiles.values()))
-
-
-    def iterate(self):
-        neighboors = collections.defaultdict(int)
-        new_tiles = self.tiles.copy()
-
-        for index, tile in new_tiles.items():
+        new_tiles = []
+        for index, tile in self.tiles.items():
             vectors = [
                 (-1, 0),
                 (0, -1),
@@ -161,16 +76,49 @@ class Floor:
 
             for vector in vectors:
                 test_index = (index[0] + vector[0], index[1] + vector[1])
-                neighboors[index] += self.tiles[test_index]
+                if test_index not in self.tiles:
+                    new_tiles.append(test_index)
 
-        for index, tile in new_tiles.items():
-            if tile:
+        for vector in new_tiles:
+            self.tiles[vector] = 0
+
+    @property
+    def black_tiles(self):
+        return sum(filter(lambda x: x == 1, self.tiles.values()))
+
+    def iterate(self):
+        neighboors = collections.defaultdict(int)
+        new_tiles = []
+
+        for index, tile in self.tiles.items():
+            vectors = [
+                (-1, 0),
+                (0, -1),
+                (1, -1),
+                (1, 0),
+                (0, 1),
+                (-1, 1),
+            ]
+
+            for vector in vectors:
+                test_index = (index[0] + vector[0], index[1] + vector[1])
+                if test_index in self.tiles:
+                    neighboors[index] += self.tiles[test_index]
+                else:
+                    new_tiles.append(test_index)
+
+        for vector in new_tiles:
+            self.tiles[vector] = 0
+
+        for index, tile in self.tiles.items():
+            if tile == 1:
                 if neighboors[index] == 0 or neighboors[index] > 2:
                     self.tiles[index] = 0
-            else:
+            elif tile == 0:
                 if neighboors[index] == 2:
                     self.tiles[index] = 1
-
+            else:
+                print('WTF ?')
 
 
 def compute_black_tiles(data):
@@ -178,12 +126,13 @@ def compute_black_tiles(data):
     floor.setup()
     return floor.black_tiles
 
+
 def compute_black_tiles_iteration(data):
     floor = Floor(data)
     floor.setup()
     for i in range(100):
         floor.iterate()
-        input(f'iteration {i+1} black {floor.black_tiles}')
+        print(f'iteration {i+1} black {floor.black_tiles}')
     return floor.black_tiles
 
 
@@ -210,11 +159,11 @@ def part1():
 
 def part2():
     data = load_data()
-    result = None
+    result = compute_black_tiles_iteration(data)
     print(f'part2 is {result}')
 
 
 test_part1()
 part1()
 test_part2()
-#part2()
+part2()

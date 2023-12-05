b9b35fb838e50738344ec04348e561df861f1f41
Anthony Martinet <anthony@alkivi.fr>
2023-12-05 12:14:42 +0100

    2023/04 part2

--- 2023/04/solve.py
+++ 2023/04/solve.py
@@ -18,26 +18,67 @@ def load_data():
     return data
 
 
-def solve_part1(data):
-    total = 0
-    for line in data:
-        card_data = line.split(':')[1].strip()
-        winners_numbers = list(map(lambda x: int(x.strip()), filter(lambda x: x, card_data.split('|')[0].split(' '))))
-        current_numbers = list(map(lambda x: int(x.strip()), filter(lambda x: x, card_data.split('|')[1].split(' '))))
+class Card:
+    def __init__(self, id, winners_numbers, current_numbers):
+        self.id = id
+        self.winners_numbers = winners_numbers
+        self.current_numbers = current_numbers
+        self.copies = 1
+
+    @property
+    def matching_numbers(self):
+        return len(set(self.current_numbers).intersection(set(self.winners_numbers)))
+
+    @property
+    def score(self):
         correct = 0
-        for number in current_numbers:
-            if number in winners_numbers:
+        for number in self.current_numbers:
+            if number in self.winners_numbers:
                 if correct == 0:
                     correct += 1
                 else:
                     correct *= 2
-        total += correct
+        return correct
+
+
+def solve_part1(data):
+    total = 0
+    cards = {}
+    for line in data:
+        card_id = int(line.split(':')[0].strip()[5:].strip())
+        card_data = line.split(':')[1].strip()
+        winners_numbers = list(map(lambda x: int(x.strip()), filter(lambda x: x, card_data.split('|')[0].split(' '))))
+        current_numbers = list(map(lambda x: int(x.strip()), filter(lambda x: x, card_data.split('|')[1].split(' '))))
+        card = Card(card_id, winners_numbers, current_numbers)
+        cards[card_id] = card
+
+    for card_id, card in cards.items():
+        total += card.score
     return total
 
 
 def solve_part2(data):
-    pass
+    cards = {}
+    for line in data:
+        card_id = int(line.split(':')[0].strip()[5:].strip())
+        card_data = line.split(':')[1].strip()
+        winners_numbers = list(map(lambda x: int(x.strip()), filter(lambda x: x, card_data.split('|')[0].split(' '))))
+        current_numbers = list(map(lambda x: int(x.strip()), filter(lambda x: x, card_data.split('|')[1].split(' '))))
+        card = Card(card_id, winners_numbers, current_numbers)
+        cards[card_id] = card
 
+    for card_id in sorted(cards.keys()):
+        card = cards[card_id]
+        matching_numbers = card.matching_numbers
+        for test in range(card.copies):
+            for to_add_index in range(card_id+1, card_id+matching_numbers+1):
+                cards[to_add_index].copies += 1
+
+        
+    total = 0
+    for card_id, card in cards.items():
+        total += card.copies
+    return total
 
 def test_part1():
     data = test_data
@@ -50,13 +91,14 @@ def part1():
     data = load_data()
     result = solve_part1(data)
     print(f'part1 is {result}')
+    assert result == 25183
 
 
 def test_part2():
     data = test_data
     result = solve_part2(data)
     print(f'test2 is {result}')
-    assert result == 25
+    assert result == 30
 
 
 def part2():
@@ -67,5 +109,5 @@ def part2():
 
 test_part1()
 part1()
-#test_part2()
-#part2()
+test_part2()
+part2()
